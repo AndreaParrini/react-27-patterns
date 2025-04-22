c4652ac5872e733c84e96180c7a4ac2556e25690
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-04-22 10:48:23 +0200

    add context in accordion components

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,5 +1,39 @@
+import { useState, createContext, useContext } from "react";
+
+const AccordionContext = createContext();
+
+export function useAccordionContext(){
+    const ctx= useContext(AccordionContext);
+
+    if(!ctx){
+        throw new Error('Accordion-related components must be wrapped by <Accordion>')
+    }
+
+    return ctx;
+}
+
 export default function Accordion({className, children}){
-    return <ul className={className}> 
-        {children}
-    </ul>
+    const [openItemId, setOpenItemId] = useState(null)
+
+    function openItem(id){
+        setOpenItemId(id)
+    }
+
+    function closeItem(){
+        setOpenItemId(null)
+    }
+
+    const ctxValue={
+        openItemId,
+        openItem,
+        closeItem
+    }
+    return (
+    <AccordionContext.Provider value={ctxValue}>
+        <ul className={className}> 
+            {children}
+        </ul>
+    </AccordionContext.Provider>
+    )
+    
 }
