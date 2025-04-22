526ff77b9ec72f5881efe683eac6f8e7187bee59
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-04-22 10:49:17 +0200

    add use context in the accordion item and add id in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ function App() {
   return <main>
     <section>
       <Accordion className='accordion'>
-        <AccordionItem className='accordion-item' title='We got 20 years of experience'>
+        <AccordionItem id='experience' className='accordion-item' title='We got 20 years of experience'>
           <article>
             <p>You can&apos;t go wrong whit us.</p>
             <p>We are in the business of planning highly individualized vacation trips for more than 20 years.</p>
           </article>
         </AccordionItem>
-        <AccordionItem className='accordion-item' title="We're working whit local guides">
+        <AccordionItem id='local-guides' className='accordion-item' title="We're working whit local guides">
           <article>
             <p>We are not doing this along from our office</p>
             <p>Instead, we are working with local guides to ensure a safe and pleasant vacation.</p>
