f28aa997e7f90d53022005dbdb4862edded7d3a8
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-04-22 12:32:51 +0200

    add function to manage the debouncing

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function SearchableList({items, itemKeyFn, children}){
+    const lastChange = useRef();
     const [searchTerm, setSearchTerm] = useState('');
 
     const searcResults = items.filter((item) => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()))
 
     function handleChange(event){
-        setSearchTerm(event.target.value)
+    // con questo codice evitiamo il Debouncing, ovvero aggiornare lo stato ad ogni pressione di un nuovo tasto
+    // con questa app non dava problemi ma con una app in cui si inviano richiesto al backend potrebbe creare dei problemi.
+    // in questo modo si aggiorna lo stato SOLO passati i 500 millesecondi dall'ultima pressione dell'utente.
+
+        // quando si cambia lo stato si controlla se c'è un timer attivo, 
+        // se c'è lo si cancella in quanto vuol dire che non sono passati i 500 millesencodi dall'ultima modifica
+       if(lastChange.current){
+            clearTimeout(lastChange.current);
+        }
+        
+        // e qui poi si reimposta il timer
+        lastChange.current = setTimeout(()=> {
+            lastChange.current = null;
+            setSearchTerm(event.target.value);
+        }, 500)  
     }
 
     return(
